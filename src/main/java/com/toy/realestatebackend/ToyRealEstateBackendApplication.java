209735e8b0f046c7dd0400c2f38f88cb2f76b65c
DESKTOP-273P83C\김진용 <yong6302@gmail.com>
2023-06-05 19:12:06 +0900

    Backend 프로젝트 생성

---
+++ src/main/java/com/toy/realestatebackend/ToyRealEstateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.toy.realestatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyRealEstateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyRealEstateBackendApplication.class, args);
+    }
+
+}
