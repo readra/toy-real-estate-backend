64517c5c8855477c5351b67459670afcbf7e25cd
DESKTOP-273P83C\김진용 <yong6302@gmail.com>
2023-11-22 23:52:12 +0900

    1. 공공데이터포털 API(HousePriceTrendSvc) 호출 테스트 코드 작성

--- src/test/java/com/toy/realestatebackend/WebFluxTest.java
+++ src/test/java/com/toy/realestatebackend/WebFluxTest.java
@@ -2,6 +2,7 @@ package com.toy.realestatebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,6 +22,21 @@ public class WebFluxTest {
         System.out.println("[ACTUAL]" + response);
         System.out.println("[EXPECTED]" + willResponse);
         assertThat(response).isEqualTo(willResponse);
+
+        DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory();
+        defaultUriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
+
+        String realEstate = WebClient.builder()
+                .uriBuilderFactory(defaultUriBuilderFactory)
+                .build()
+                .get()
+                .uri("https://api.odcloud.kr/api/HousePriceTrendSvc/v1/getHousePriceIndex?region_cd=11000&research_date=202202&apt_type=0&tr_gbn=T&serviceKey=%2F7MeSbybd07ucEmj8BF72GmhsZV9KbqQ2BTpylshbKDKGNzSktYgCYvTOkvKuZCxWc8WHA5B3ecQ9qld7%2BGjOw%3D%3D")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println("REAL-ESTATE]" + realEstate);
+
         System.out.println("WebFlux Test Completed.");
     }
 }
