3137f7f99b544e49dd930d2e02d02fed19c0c0d8
DESKTOP-273P83C\김진용 <yong6302@gmail.com>
2023-12-03 21:46:28 +0900

    1. 기간 조회 구현 중

--- src/test/java/com/toy/realestatebackend/AptTradeTest.java
+++ src/test/java/com/toy/realestatebackend/AptTradeTest.java
@@ -16,12 +16,33 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 public class AptTradeTest {
     @Test
     public void aptTradeTest() throws IOException, ParserConfigurationException, SAXException {
+        String yyyyMM = "201512";
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMM");
+
+        try {
+            Calendar calendar = Calendar.getInstance();
+            Date date = simpleDateFormat.parse(yyyyMM);
+
+            calendar.setTime(date);
+
+            calendar.add(Calendar.MONTH, 1);
+
+            System.out.println(simpleDateFormat.format(calendar.getTime()));
+        } catch ( Exception e ) {
+            System.out.println("Failed to parse date format.");
+        }
+
+
         StringBuilder stringBuilder = new StringBuilder("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade"); /*URL*/
         stringBuilder.append("?" + URLEncoder.encode("serviceKey", StandardCharsets.UTF_8) + "=" + "%2F7MeSbybd07ucEmj8BF72GmhsZV9KbqQ2BTpylshbKDKGNzSktYgCYvTOkvKuZCxWc8WHA5B3ecQ9qld7%2BGjOw%3D%3D"); /*Service Key*/
         stringBuilder.append("&" + URLEncoder.encode("LAWD_CD", StandardCharsets.UTF_8) + "=" + URLEncoder.encode("11110", StandardCharsets.UTF_8)); /*각 지역별 코드*/
