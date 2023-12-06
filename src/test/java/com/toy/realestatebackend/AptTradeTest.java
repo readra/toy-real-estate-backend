e1b8aa50ed0380632975a5ab129b1ffb0455abdf
DESKTOP-273P83C\김진용 <yong6302@gmail.com>
2023-12-06 23:58:14 +0900

    1. 시작년월 ~ 종료년월까지 아파트매매실거래 자료 조회

--- src/test/java/com/toy/realestatebackend/AptTradeTest.java
+++ src/test/java/com/toy/realestatebackend/AptTradeTest.java
@@ -8,12 +8,9 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
@@ -23,7 +20,7 @@ import java.util.List;
 
 public class AptTradeTest {
     @Test
-    public void aptTradeTest() throws IOException, ParserConfigurationException, SAXException {
+    public void aptTradeTest() {
         String startYyyyMm = "20151201";
         String endYyyyMm = "20161001";
 
@@ -42,62 +39,52 @@ public class AptTradeTest {
                 System.out.println(nowLocalDate.getYear() + month);
 
                 nowLocalDate = nowLocalDate.plusMonths(1);
+
+                StringBuilder stringBuilder = new StringBuilder("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade"); /*URL*/
+                stringBuilder.append("?" + URLEncoder.encode("serviceKey", StandardCharsets.UTF_8) + "=" + "%2F7MeSbybd07ucEmj8BF72GmhsZV9KbqQ2BTpylshbKDKGNzSktYgCYvTOkvKuZCxWc8WHA5B3ecQ9qld7%2BGjOw%3D%3D"); /*Service Key*/
+                stringBuilder.append("&" + URLEncoder.encode("LAWD_CD", StandardCharsets.UTF_8) + "=" + URLEncoder.encode("11110", StandardCharsets.UTF_8)); /*각 지역별 코드*/
+                stringBuilder.append("&" + URLEncoder.encode("DEAL_YMD", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(nowLocalDate.getYear() + month, StandardCharsets.UTF_8)); /*월 단위 신고자료*/
+                stringBuilder.append("&" + URLEncoder.encode("pageNo", StandardCharsets.UTF_8) + "=" + URLEncoder.encode("2", StandardCharsets.UTF_8));
+
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document document = builder.parse(stringBuilder.toString());
+                document.getDocumentElement().normalize();
+
+                List<Item> items = new LinkedList<>();
+                NodeList nodeList = document.getElementsByTagName("item");
+                for ( int i = 0; i < nodeList.getLength(); i++ ) {
+                    Node node = nodeList.item(i);
+                    if ( node.getNodeType() == Node.ELEMENT_NODE ) {
+                        Element element = (Element) node;
+
+                        items.add(
+                                Item.builder()
+                                        .transactionAmount(getElementByTagName(element, "거래금액"))
+                                        .transactionType(getElementByTagName(element, "거래유형"))
+                                        .buildingYear(Integer.parseInt(getElementByTagName(element, "건축년도")))
+                                        .year(Integer.parseInt(getElementByTagName(element, "년")))
+                                        .month(Integer.parseInt(getElementByTagName(element, "월")))
+                                        .day(Integer.parseInt(getElementByTagName(element, "일")))
+                                        .registrationDate(getElementByTagName(element, "등기일자"))
+                                        .legalBuilding(getElementByTagName(element, "법정동"))
+                                        .apartmentName(getElementByTagName(element, "아파트"))
+                                        .exclusiveArea(Double.parseDouble(getElementByTagName(element, "전용면적")))
+                                        .agencyLocation(getElementByTagName(element, "중개사소재지"))
+                                        .localNumber(getElementByTagName(element, "지번"))
+                                        .lawdCode(Integer.parseInt(getElementByTagName(element, "지역코드")))
+                                        .layer(Integer.parseInt(getElementByTagName(element, "층")))
+                                        .liftReasonDate(getElementByTagName(element, "해제사유발생일"))
+                                        .isLift(Boolean.parseBoolean(getElementByTagName(element, "해제여부")))
+                                        .build()
+                        );
+                    }
+                }
+
+                for ( Item item : items ) System.out.println(item.toString());
             }
         } catch ( Exception e ) {
             System.out.println("Failed to parse date format." + e);
-        }
-
-        StringBuilder stringBuilder = new StringBuilder("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade"); /*URL*/
-        stringBuilder.append("?" + URLEncoder.encode("serviceKey", StandardCharsets.UTF_8) + "=" + "%2F7MeSbybd07ucEmj8BF72GmhsZV9KbqQ2BTpylshbKDKGNzSktYgCYvTOkvKuZCxWc8WHA5B3ecQ9qld7%2BGjOw%3D%3D"); /*Service Key*/
-        stringBuilder.append("&" + URLEncoder.encode("LAWD_CD", StandardCharsets.UTF_8) + "=" + URLEncoder.encode("11110", StandardCharsets.UTF_8)); /*각 지역별 코드*/
-        stringBuilder.append("&" + URLEncoder.encode("DEAL_YMD", StandardCharsets.UTF_8) + "=" + URLEncoder.encode("201512", StandardCharsets.UTF_8)); /*월 단위 신고자료*/
-        stringBuilder.append("&" + URLEncoder.encode("pageNo", StandardCharsets.UTF_8) + "=" + URLEncoder.encode("2", StandardCharsets.UTF_8));
-
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(stringBuilder.toString());
-        document.getDocumentElement().normalize();
-
-        List<Item> items = new LinkedList<>();
-        NodeList nodeList = document.getElementsByTagName("item");
-        for ( int i = 0; i < nodeList.getLength(); i++ ) {
-            Node node = nodeList.item(i);
-            if ( node.getNodeType() == Node.ELEMENT_NODE ) {
-                Element element = (Element) node;
-
-                items.add(
-                        Item.builder()
-                                .transactionAmount(getElementByTagName(element, "거래금액"))
-                                .transactionType(getElementByTagName(element, "거래유형"))
-                                .buildingYear(Integer.parseInt(getElementByTagName(element, "건축년도")))
-                                .year(Integer.parseInt(getElementByTagName(element, "년")))
-                                .month(Integer.parseInt(getElementByTagName(element, "월")))
-                                .day(Integer.parseInt(getElementByTagName(element, "일")))
-                                .registrationDate(getElementByTagName(element, "등기일자"))
-                                .legalBuilding(getElementByTagName(element, "법정동"))
-                                .apartmentName(getElementByTagName(element, "아파트"))
-                                .exclusiveArea(Double.parseDouble(getElementByTagName(element, "전용면적")))
-                                .agencyLocation(getElementByTagName(element, "중개사소재지"))
-                                .localNumber(getElementByTagName(element, "지번"))
-                                .lawdCode(Integer.parseInt(getElementByTagName(element, "지역코드")))
-                                .layer(Integer.parseInt(getElementByTagName(element, "층")))
-                                .liftReasonDate(getElementByTagName(element, "해제사유발생일"))
-                                .isLift(Boolean.parseBoolean(getElementByTagName(element, "해제여부")))
-                                .build()
-                );
-            }
-        }
-
-        for ( Item item : items ) System.out.println(item.toString());
-
-        NodeList pageNo = document.getElementsByTagName("body");
-        for ( int i = 0; i < pageNo.getLength(); i++ ) {
-            Node node = pageNo.item(i);
-            if ( node.getNodeType() == Node.ELEMENT_NODE ) {
-                Element element = (Element) node;
-
-                System.out.println(getElementByTagName(element, "pageNo"));
-            }
         }
     }
 
