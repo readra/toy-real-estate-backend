d3b3cdb0d9d4c38d24dffb9ca399f36d48bfd3ae
DESKTOP-273P83C\김진용 <yong6302@gmail.com>
2023-12-29 21:51:35 +0900

    1. 아파트 매매 신고정보 조회 Controller 테스트 코드 내, RequestParam 추가 2. Response 값 확인 TODO#1. Response 검증 방법 확인 (ex. 객체 타입 혹은 일부 데이터 일치 확인 등)

--- src/test/java/com/toy/realestatebackend/controller/AptTradeControllerTest.java
+++ src/test/java/com/toy/realestatebackend/controller/AptTradeControllerTest.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.nio.charset.StandardCharsets;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,7 +26,14 @@ public class AptTradeControllerTest {
     @Test
     @DisplayName("아파트 매매 신고정보 조회 테스트")
     void apartment_test() throws Exception {
-        mockMvc.perform(get(BASE_URL + "/apartment"))
-                .andExpect(status().isOk());
+        MvcResult mvcResult = mockMvc.perform(get(BASE_URL + "/apartment?lawdCode={lawdCode}&startYearMonth={startYearMonth}&endYearMonth={endYearMonth}",
+                        11740,
+                        YearMonth.parse("201501", DateTimeFormatter.ofPattern("yyyyMM")),
+                        YearMonth.parse("201501", DateTimeFormatter.ofPattern("yyyyMM"))))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        System.out.println(content);
     }
 }
