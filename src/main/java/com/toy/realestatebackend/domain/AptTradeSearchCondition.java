ca488e7e91edb9e38c4a5be038ff9d6543a189d3
DESKTOP-273P83C\김진용 <yong6302@gmail.com>
2024-01-01 16:44:42 +0900

    1. 아파트 매매 신고정보 조회 조건 객체 내, @DateTimeFormat, @JsonFormat 제거 (YearMonth 대상 어노테이션 미지원 : https://github.com/spring-projects/spring-framework/pull/1215)

--- src/main/java/com/toy/realestatebackend/domain/AptTradeSearchCondition.java
+++ src/main/java/com/toy/realestatebackend/domain/AptTradeSearchCondition.java
@@ -1,10 +1,8 @@
 package com.toy.realestatebackend.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.YearMonth;
 
@@ -19,12 +17,8 @@ public class AptTradeSearchCondition {
     // 지역명
     private final int lawdCode;
     // 조회년월 (시작)
-    @DateTimeFormat(pattern = "yyyyMM")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMM", timezone = "Asia/Seoul")
     private final YearMonth startYearMonth;
     // 조회년월 (종료)
-    @DateTimeFormat(pattern = "yyyyMM")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMM", timezone = "Asia/Seoul")
     private final YearMonth endYearMonth;
 
     @Builder
