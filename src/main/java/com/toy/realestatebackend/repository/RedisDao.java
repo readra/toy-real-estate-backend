8f5ff4c4a9f58634f17297523981b815e8f96d2f
DESKTOP-273P83C\김진용 <yong6302@gmail.com>
2024-02-04 18:12:03 +0900

    1. Redis 적용 - RedisDao 주석 수정

--- src/main/java/com/toy/realestatebackend/repository/RedisDao.java
+++ src/main/java/com/toy/realestatebackend/repository/RedisDao.java
@@ -23,7 +23,7 @@ public class RedisDao {
     }
 
     /**
-     * ValueOperations 통한 단일 데이터 입력
+     * ValueOperations 통한 데이터 입력
      *
      * @param key
      *      데이터 key
@@ -36,7 +36,7 @@ public class RedisDao {
     }
 
     /**
-     * ValueOperations 통한 리스트 데이터 입력
+     * ListOperations 통한 데이터 입력
      *
      * @param key
      *      데이터 key
